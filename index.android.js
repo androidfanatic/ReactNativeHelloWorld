a364623bd282685614249004b1bd84b1ff60a994
Android Fanatic <manish_raj@outlook.com>
2016-11-04 01:05:50 +0530

    some styling

--- index.android.js
+++ index.android.js
@@ -1,14 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 // Hello World App - the ancient traditions of our people
 
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
 
 class ReactNativeHelloWorld extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View style={styles.container}>
+	<Text style={styles.hello}>Hello World!</Text>
+      </View>
     );
   }
 }
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  hello: {
+    fontSize: 40,
+    textAlign: 'center',
+    color: '#aaaaaa'
+  },
+});
+
 AppRegistry.registerComponent('ReactNativeHelloWorld', () => ReactNativeHelloWorld);
+
