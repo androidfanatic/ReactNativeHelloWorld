d2def94c17362eb4e15d3d20994be04a936ae04c
Android Fanatic <manish_raj@outlook.com>
2016-11-04 23:15:35 +0530

    color change

--- index.android.js
+++ index.android.js
@@ -1,30 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 // Hello World App - the ancient traditions of our people
 
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+import { AppRegistry, Text, View, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
 
 class ReactNativeHelloWorld extends Component {
   render() {
     return (
       <View style={styles.container}>
-	<Text style={styles.hello}>Hello World!</Text>
+        <TouchableOpacity>
+    	   <Text style={styles.hello}>
+          Hello World!
+         </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#333',
   },
   hello: {
     fontSize: 40,
@@ -34,4 +32,3 @@ const styles = StyleSheet.create({
 });
 
 AppRegistry.registerComponent('ReactNativeHelloWorld', () => ReactNativeHelloWorld);
-
